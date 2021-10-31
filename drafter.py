07c15726616e25cb8383f2f99c4b6487df8ced28
Austin Smith <smithaustin0129@gmail.com>
2021-10-31 13:36:36 -0400

    wip

--- drafter.py
+++ drafter.py
@@ -15,10 +15,12 @@ required_pos = {
 def get_snake_picks(num_teams, rounds):
     picks = []
     snake = False
+    total = 0
     for r in range(rounds):
         order = range(num_teams) if not snake else range(num_teams)[::-1]
         for team in order:
-            picks.append((team, r))
+            picks.append((total, team, round))
+            total += 1
         snake = not snake
     return picks
 
@@ -67,12 +69,14 @@ def create_teams(num_teams):
         teams[i] = {'name': '', 'players': []}
     return teams
 
-def ucb(node):
+def calculate_ucb(node):
+    if node.visited == 0:
+        return None
     vi = sum([child.score for child in node.children]) / len(node.children) if len(node.children) > 0 else 0
     return vi + (2 * math.sqrt(math.log(node.parent.visited) / node.visited))
 
 class Node:
-    def __init__(self, draftboard, taken, team, parent):
+    def __init__(self, draftboard, taken, pick, parent):
         qb = pick_best_player_at_pos('QB', draftboard, taken, team)
         rb = pick_best_player_at_pos('RB', draftboard, taken, team)
         wr = pick_best_player_at_pos('WR', draftboard, taken, team)
@@ -83,9 +87,9 @@ class Node:
         self.score = 0
         self.visited = 0
         self.taken = []
-        self.team = []
+        self.team = team
+        self.team_rosters = {} 
         self.parent = parent
-        next_pick = None
         self.children = [
             Node(draftboard, self.taken + [qb], next_pick, parent),
             Node(draftboard, self.taken + [rb], next_pick, parent),
@@ -97,21 +101,61 @@ class Node:
         ]
 
 def is_terminal(node):
-    pass
+    for team in node.team_rosters:
+        required = required_pos.copy()
+        for player in team:
+            required[player.pos] -= 1
+        if required.values().count(0) != len(required_pos.keys()):
+            return False
+    return True
 
-def rollout(node):
-    while not is_terminal(node):
-        action = random.choice(node.children)
+def rollout(node, draftboard):
+    picks = get_snake_picks(10, 8)
+    last_pick = picks[-1][0]
+    reward = 0
+    simming_team = node.team
+    team = simming_team
+    while node.pick <= last_pick: 
+        picking_team = picks[node.pick][1]
+        player = random.choice(node.children)
+        if picking_team == simming_team:
+            reward += player.points
+        node = Node(draftboard, node.taken + [player], node.pick+1, node)
+    return reward
+
+def choose_child(children):
+    for child in children:
+        ucb = calculate_ucb(child)
+        if not ucb:
+            chosen_node = child
+            break
+        else:
+            if ucb > max_usb:
+                max_ucb = ucb
+                chosen_node = child
+    return chosen_node
+
+def backpropogate_value(node, value):
+    if not node:
+        return
+    if value > node.value:
+        node.value = value
+        backpropogate_value(node.parent, value)
 
 if __name__ == '__main__':
     NUM_TEAMS = 10
     ITERATIONS = 1000
     draftboard = get_draftboard()
     teams = create_teams(NUM_TEAMS)
-    picks = get_snake_picks(NUM_TEAMS, 9)
-
-    root = Node(draftboard, [], 0, None)
+    picks = get_snake_picks(NUM_TEAMS, 8)
+    # print (picks)
+    node = Node(draftboard, [], picks[0][0], None)
     for i in range(ITERATIONS):
-       # rollout 
-       while not is_terminal(current_node):
-
+        node_to_explore = choose_child(node.children)
+        if is_leaf(node_to_explore):
+            rollout_value = rollout(node_to_explore, draftboard)
+            backpropogate_value(node_to_explore, rollout_value)
+        else:
+            # Add new child node with populated actions
+            # and then roll out from it
+            pass
